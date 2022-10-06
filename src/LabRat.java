98ee9c28ac1498bb2c47ae74e7032b2e133b9250
BT_4N2_14 <hsun7920@bths.edu>
2022-10-06 12:09:39 -0400

    Initial commit

---
+++ src/LabRat.java
@@ -0,0 +1,8 @@
+public class LabRat {
+    public static void main(String[] args) {
+        String blah = "What’s for dinner?";
+        String bleh = blah.substring(11,17);
+        System.out.println(bleh);
+
+    }
+}
